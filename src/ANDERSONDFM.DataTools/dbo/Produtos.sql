063adfa41f0897babcf63728eb502b68391bdc75
andersondfm <anderson.migloranza@gmail.com>
2022-03-24 16:16:45 -0300

    Criação de Tabela.

---
+++ src/ANDERSONDFM.DataTools/dbo/Produtos.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Produtos]
+(
+    [Id] [int] NOT NULL,
+	[Nome] [varchar](150) NOT NULL,
+	[DataInclusao] [datetime2](7) NOT NULL,
+	[UsuarioInclusao] [varchar](50) NOT NULL,
+    CONSTRAINT [PK_GrauParentesco] PRIMARY KEY CLUSTERED ([Id] ASC)
+)
